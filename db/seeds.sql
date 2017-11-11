98671cbc93a2153d1a056a033bcf86a244dcdbb2
ewok1138 <mbagdazian90@gmail.com>
2017-11-10 19:32:02 -0800

    EatTHEBurger

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+
+
+
+-- Seed Burgers into Database ... No Sesame! (hehehe)
+INSERT INTO burgers(burger_name, devoured, date)
+VALUES ("Big Mac", false, "2016-11-05 04:20:00"),
+  ("Tri Tip Sammy", false, "2016-11-05 16:20:00"),
+  ("SteakBurger", false, "2016-11-05 16:20:01"),
+  ("Santa Barbara Char", false, "2016-11-05 04:20:20"),
+  ("Double Double - animal style", true, "2016-11-05 16:20:16");
